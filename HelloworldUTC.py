0c08a8edae6b7ce2c45536308a9a074e5ce2a99a
rudascristian <42628627+rudascristian@users.noreply.github.com>
2018-08-28 13:21:18 -0400

    Add files via upload

---
+++ HelloworldUTC.py
@@ -0,0 +1,16 @@
+
+
+
+print('hello world')
+
+x = int(input("Enter an integer: "))
+if x%2 ==0:
+    print('')
+    print('even')
+else:
+    print('')
+    print('odd')
+print('Done with conditional')
+
+
+
